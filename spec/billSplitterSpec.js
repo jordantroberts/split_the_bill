b8e7433f03ba071badcc394179f3d29e5e0ca793
Jordan Roberts <jordanroberts@talk21.com>
2019-07-09 15:02:30 +0100

    Basic HTML form with backend logic implemented

---
+++ spec/billSplitterSpec.js
@@ -0,0 +1,11 @@
+describe('Bill_Splitter', function() {
+  var splitter;
+
+  beforeEach(function(){
+    splitter = new Bill_Splitter()
+  });
+
+  describe('')
+
+
+}
---
+++ src/billSplitter.js
@@ -0,0 +1,18 @@
+function Bill_Splitter(){
+
+  this.amount = 0;
+
+};
+
+  Bill_Splitter.prototype.calculate = function(){
+
+    var cost = parseInt(document.getElementById("cost").value);
+    var tip = parseInt(document.getElementById("tip").value);
+    var diners = parseInt(document.getElementById("diners").value);
+
+    var amount = (cost + tip) / diners
+
+    this.amount += amount
+    document.getElementById("amount").innerHTML = amount
+
+  }
