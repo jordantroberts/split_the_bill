5a84e072a41bbe382fa64551aed8ec8f2d843484
Jordan Roberts <jordanroberts@talk21.com>
2019-07-10 12:17:02 +0100

    Finished README

--- src/billSplitter.js
+++ src/billSplitter.js
@@ -13,17 +13,16 @@ Bill_Splitter.prototype.calculate = function(){
 
   var amount = (this.cost + this.tip) / this.diners
 
-    if(document.getElementById("cost").value.length == 0 {
-        alert("Please enter the cost of your meal")
-        return false;
-    } else if(document.getElementById("tip").value.length == 0) {
-        alert("Please enter a tip amount. If you don't want to tip, please type '0'")
-        return false;
-    } else if(document.getElementById("diners").value.length == 0) {
-        alert("Please enter the amount of diners")
-        return false;
-    } else {
+    if(document.getElementById("cost").value.length == 0) {
+      alert("Please enter the cost of your meal")
+      return false;
+  } else if(document.getElementById("tip").value.length == 0) {
+      alert("Please enter a tip amount. If you don't want to tip, please type '0'")
+      return false;
+  } else if(document.getElementById("diners").value.length == 0) {
+      alert("Please enter the amount of diners")
+      return false;
+  } else {
       this.amount = amount
-
   }
 }
